accd14a7823b7681916f6f4a274ab7fca3890295
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-02 18:33:34 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.zrua0aj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Welcome to Programming Hero Cart Doctor Server');
+});
+app.listen(port, () => {
+  console.log(`server listening at ${port}`);
+});
